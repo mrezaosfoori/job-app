afac082d1b88e775c3a6edfaadff72bfd9074f19
M.Reza Osfoori <m.osfoori@vandabg.com>
2025-04-15 16:15:53 +0330

    edit some css

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,22 +12,21 @@ function Hero() {
     <div className=" flex   w-full flex-col items-center justify-center mb-[150px]  " >
       {/* Radial gradient for the container to give a faded look */}
 
-      <div className=" flex flex-col md:flex-row gap-2  p-4    ">
+      <div className=" flex flex-col md:flex-row gap-[2px] md:gap-2  p-1 md:p-4    ">
         <section className="flex flex-1 items-center justify-center">
           <Image
             src={hero}
             alt="hero image"
-            className=" bottom-0 z-30 h-full w-full object-cover"
+            className=" bottom-0 z-30 h-full w-full object-cover bg-red-300 bg-opacity-5"
           />
         </section>
         <section className="z-[9999]  flex-1 opacity-95">
-          <p className="text-[50px]  ">
+          <p className="text-[24px] sm:text-[22px] md:text-[40px] xl:text-[46px]  ">
             کاری پیدا کن
-            <br />
-            که با علایق و مهارت‌هات هم‌خوانی داشته باشه
+          که مطابق علاقت باشه...
           </p>
 
-          <p className="mt-8 text-[18px] text-slate-500 ">
+          <p className="mt-2 md:mt-8 text-[16px] sm:text-[18px] text-slate-500 ">
             هزاران فرصت شغلی در تمام بخش‌های پیشرو منتظر تو هستند...
           </p>
         </section>
@@ -36,22 +35,22 @@ function Hero() {
       </div>
       <div className="flex w-full flex-col  gap-4 bg-red-900  bg-opacity-5  ">
         <div className="flex flex-col md:flex-row gap-3 items-center  p-4  md:h-[100px] ">
-        <div className="flex  p-2 bg-white w-full ">
-            <div className="relative md:min-w-[300px] flex-1 ">
+        <div className="flex  p-2  w-full bg-white border ">
+        <div className="flex md:min-w-[300px]  flex-1  p-1 items-center gap-2 ">
               <Image
                 src={search}
                 alt="hero image"
-                className=" absolute right-2 top-1/2 translate-y-[-50%] w-6 h-6"
+                    className=" w-6 h-6"
               />
               <input type="text" placeholder="عنوان ..." className=" p-4 w-full h-full placeholder:absolute placeholder:right-10" />
             </div>
-            <div className="relative md:min-w-[300px] border-r-2 flex-1 ">
+            <div className="flex md:min-w-[300px] border-r-2 flex-1  p-1 items-center gap-2 ">
               <Image
                 src={location}
                 alt="hero image"
-                className=" absolute right-2  top-1/2 translate-y-[-50%] w-6 h-6"
+                className=" w-6 h-6"
               />
-              <input type="text" placeholder="مکان ..." className=" p-4 w-full h-full placeholder:absolute placeholder:right-10"  />
+              <input type="text" placeholder="مکان ..." className=" p-4  w-full h-full placeholder:absolute placeholder:right-10"  />
             </div>
           </div>
 
